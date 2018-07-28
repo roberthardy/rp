9e84bc3a53cf5fc0929f3b1ae0cb44bfaec5073d
Robert Hardy <robert.hardy@gmail.com>
2018-07-28 16:05:22 +0100

    Server side webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry : {}
+    entry : './src/server.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    target: 'node'
 }
