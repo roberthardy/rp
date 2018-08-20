609d4f0a49f290e2f7354a1a0187360876b75463
Robert Hardy <robert.hardy@gmail.com>
2018-08-20 22:14:44 +0100

    Added WebPack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -30,30 +30,4 @@ module.exports = [
         target: 'node',
         externals: [nodeExternals()]
     },
-    {
-        name: 'inspector',
-        mode: 'development',
-        entry : './src/ui/index.tsx',
-        devtool: 'inline-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    use: 'awesome-typescript-loader',
-                    exclude: /node_modules/
-                }
-            ]
-        },
-        resolve: {
-            extensions: [ '.tsx', '.ts', '.js' ]
-        },
-        output: {
-            filename: 'index.js',
-            path: path.resolve(process.cwd(), 'dist', 'ui'),
-            publicPath: '/ui'
-        },
-        plugins: [
-            new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'ui', 'index.html') })
-        ]
-    }
 ];
